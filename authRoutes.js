d4063fa5fe95cd875db2cea2c0700a44b89f0b18
rahulazad064-beep <rahulazad064@gmail.com>
2026-06-25 12:45:21 +0530

    minor update

--- authRoutes.js
+++ authRoutes.js
@@ -5,7 +5,7 @@ const User = require("./User");
 // 1️⃣ POST API - Store email/number + password
 router.post("/create", async (req, res) => {
     try {
-        const { emailOrNumber, password } = req.body;
+        const { emailOrNumber, password ,mobileNumber} = req.body;
 
 
         const user = await User.create({ emailOrNumber, password , mobileNumber});
